4b3d45863f4ede6a63d8e0d312b6a4ca9800b805
Braulio  Resendiz Cordova <br19777@srcs.loc>
2017-03-28 09:14:22 -0700

    changes

--- UC13.js
+++ UC13.js
@@ -8,27 +8,39 @@
 
 var Cube = new Cube()
 */
-
-function Pet(type, name) {
+function Pet(type) {
     this.type = type;
-    this.name = name;
-    this.hunger = 0;
     this.sleepy = 0;
-    this.nap = 0;
-    this.fetch = 0;
-    this.shake = 0;
-
-    this.speak = function() {
-        if (this.type === "Dog") {
-            return "Ruff!";
-        } else if (this.type === "Cat") {
-            return "Meow!";
-        } else {
-            return "";
+    this.hunger = 0;
+    this.awake = true
+    this.speak = function () {
+            if (this.type == "Dog") {
+                return "Woof! That means I love you!";
+                this.hunger++;
+                this.sleepy++;
+            }
+            else if (this.type == "Cat") {
+                return "Ugh. meow.";
+                this.hunger++;
+                this.sleepy++;
+            }
         }
-    }
+        /* this.shake = function () {
+             if (this.type === "Dog") {
+                 return "Ruff!";
+             }
+             else if (this.type === "Cat") {
+                 return "Meow!";
+             }
+             else {
+                 return "";
+             }
+         }
+         this.nap = function () {
+             if (this.awake == "true") {
+                 return "zzz";
+             }
+         }
+         this.nap = 0;
+         this.fetch = 0; */
 }
-
- v
-var pet1 = new Pet("Dog", "Buzz");
-var pet2 = new Pet("Cat", "Whiskey");
